5311368cd1d6d829993af41611ffd75ad7db3407
Dany cm <danycmwow@gmail.com>
2022-02-15 17:49:50 +0100

    fix: Flake8 correction

--- oc_lettings_site/tests.py
+++ oc_lettings_site/tests.py
@@ -6,7 +6,7 @@ class TestView(TestCase):
     def test_homepage_success(self):
         client = Client()
         response = client.get(reverse("index"))
-        
+
         self.assertContains(response, "<title>Holiday Homes</title>")
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "index.html")
