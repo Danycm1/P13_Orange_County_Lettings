00081369533f144b0759524a1bd9cf5eed06a066
Dany cm <danycmwow@gmail.com>
2022-03-15 17:53:59 +0100

    testing

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -14,6 +14,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
+TEST = config('TESTKEY')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
