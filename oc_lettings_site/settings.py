0df2b20cfc4550971ff147571c113347d5daeac9
Dany cm <danycmwow@gmail.com>
2022-03-15 23:14:36 +0100

    test

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -12,11 +12,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-if os.getenv('SECRET_KEY'):
+""" if os.getenv('SECRET_KEY'):
     SECRET_KEY = os.getenv('SECRET_KEY')
 else:
     load_dotenv()
-    SECRET_KEY = os.getenv('SECRET_KEY')
+    SECRET_KEY = os.getenv('SECRET_KEY') """
+
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
