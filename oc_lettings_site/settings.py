3b94642285e1f3d7bd22f9c79118f8c3ddf40df0
Dany cm <danycmwow@gmail.com>
2022-03-16 11:33:52 +0100

    Upload staticfiles

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -3,6 +3,9 @@ import django_on_heroku
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 from django.core.management.utils import get_random_secret_key
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
